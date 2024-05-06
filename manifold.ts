a456660c0fa8d712212467122a5915594cfe42f7
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-07 05:28:48 +0800

    Update manifold.ts

--- manifold.ts
+++ manifold.ts
@@ -1,16 +1,3 @@
 import './ChenClass'
-interface ChenClass {
-    Class(): any;
-}
-abstract class Class implements ChenClass{
-    Class() {
-        return 'ChenClass'    
-    }
-    multiply(g1: any, g2: any) {
-        return g1*g2;
-    }
-    getInverse(g: any) {
-        return 1 % g === 0;
-    }
-}
+
 
